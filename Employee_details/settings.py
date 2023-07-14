fba74d478be63b8c2e2847493af7d17ec5b2dcb3
Neon2k2 <gaurishninnekar@gmail.com>
2023-07-14 12:43:34 +0530

    updated UI

--- Employee_details/settings.py
+++ Employee_details/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'firstapp',
     'bootstrap5',
-    'fontawesome_5',
+
 ]
 
 MIDDLEWARE = [
@@ -126,7 +126,10 @@ L10N = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
