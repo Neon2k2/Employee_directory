94efd48fde7d23b1d574e4a7954d4b37cde0d119
Gaurish Ninnekar <44323737+Neon2k2@users.noreply.github.com>
2026-05-18 04:18:14 +0530

    Update urls.py

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
 from firstapp.views import *
+
 urlpatterns = [
-    path('login/', CustomLoginView.as_view() , name='login'),
+    path('', CustomLoginView.as_view(), name='home'),
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('employees/', EmployeeListView.as_view(), name='employee_list'),
     path('download/', DownloadEmployeesView.as_view(), name='download_employees'),
     path('downloadpdf/', DownloadEmployeesPDFView.as_view(), name='download_pdf'),
